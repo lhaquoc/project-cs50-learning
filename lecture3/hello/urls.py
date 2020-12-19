b67d66ec94ca64866276a4b5326a3f58b334e239
lhaquoc <38935979+lhaquoc@users.noreply.github.com>
2020-12-19 11:17:24 +0700

    create myProject

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:name>", views.greet, name="greet"),
-    path("quoc", views.quoc, name="quoc"),
-    path("david", views.david, name="david")
 ]
--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -2,14 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello")
-
-def quoc(request):
-    return HttpResponse("Hello, Quoc!")
-
-def david(request):
-    return HttpResponse("Hello, David!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
+
+def index(request):
+    return render(request, "hello/index.html")
